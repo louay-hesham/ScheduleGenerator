df1c1da243c7c2b17a7e9eb689abf28f9ae5c5cb
louay-hesham <louay.hesham@gmail.com>
2016-10-07 02:18:51 +0200

    Added get meetin type in GroupPanel

--- src/GUI/PreGenerator/GroupPanel.java
+++ src/GUI/PreGenerator/GroupPanel.java
@@ -114,6 +114,11 @@ public class GroupPanel extends javax.swing.JPanel {
             }
         }
     }
+    
+    public String getMeetingType(int day, int period) throws Exception{
+        this.initGroupInfo();
+        return this.groupInfo.getMeetingType(day, period);
+    }
 
     public String generateString() throws Exception {
         StringBuilder sb = new StringBuilder();
