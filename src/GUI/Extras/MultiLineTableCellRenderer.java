5c169f754629af21d7ee703e16d80bdc32c1907e
louay-hesham <louay.hesham@gmail.com>
2016-10-29 18:08:24 +0200

    removed unneeeded code

--- src/GUI/Extras/MultiLineTableCellRenderer.java
+++ src/GUI/Extras/MultiLineTableCellRenderer.java
@@ -29,35 +29,3 @@ public class MultiLineTableCellRenderer extends JList<String> implements TableCe
         return this;
     }
 }
-        
-        /*extends JTextArea implements TableCellRenderer {
-
-    public MultiLineTableCellRenderer() {
-        setLineWrap(true);
-        setWrapStyleWord(true);
-        setOpaque(true);
-    }
-
-    public Component getTableCellRendererComponent(JTable table, Object value,
-            boolean isSelected, boolean hasFocus, int row, int column) {
-        if (isSelected) {
-            setForeground(table.getSelectionForeground());
-            setBackground(table.getSelectionBackground());
-        } else {
-            setForeground(table.getForeground());
-            setBackground(table.getBackground());
-        }
-        setFont(table.getFont());
-        if (hasFocus) {
-            setBorder(UIManager.getBorder("Table.focusCellHighlightBorder"));
-            if (table.isCellEditable(row, column)) {
-                setForeground(UIManager.getColor("Table.focusCellForeground"));
-                setBackground(UIManager.getColor("Table.focusCellBackground"));
-            }
-        } else {
-            setBorder(new EmptyBorder(1, 2, 1, 2));
-        }
-        setText((value == null) ? "" : value.toString());
-        return this;
-    }
-}*/
