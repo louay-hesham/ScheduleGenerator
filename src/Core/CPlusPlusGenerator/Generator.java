a83fd9fdce7236588526ef580c0f7fd2daa7aead
louay-hesham <louay.hesham@gmail.com>
2017-02-01 22:57:42 +0200

    Fixed CPlusPlusGenerator where exe didn't run.

--- src/Core/CPlusPlusGenerator/Generator.java
+++ src/Core/CPlusPlusGenerator/Generator.java
@@ -5,10 +5,8 @@
  */
 package Core.CPlusPlusGenerator;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
+import javax.swing.*;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -43,9 +41,21 @@ public class Generator {
 
     private static String runExe(String input) {
         try {
+            //copying exe file from within the JAR to outside.
+            InputStream is = Generator.class.getResource("Schedule_Creator.exe").openStream();
+            FileOutputStream os = new FileOutputStream("Schedule_Creator.exe");
+            byte[] b = new byte[2048];
+            int length;
+            while ((length = is.read(b)) != -1) {
+                os.write(b, 0, length);
+            }
+            is.close();
+            os.close();
+            //running exe
             Runtime rt = Runtime.getRuntime();
-            URL url = Generator.class.getResource("Schedule_Creator.exe");
-            Process p = rt.exec(url.getPath());
+            String exeLocation = new File(Core.Generator.Generator.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParent() + File.separator + "Schedule_Creator.exe";
+            JOptionPane.showMessageDialog(null, exeLocation, "Error", JOptionPane.ERROR_MESSAGE);
+            Process p = rt.exec(exeLocation);
             InputStream in = p.getInputStream();
             OutputStream out = p.getOutputStream();
             //InputStream err = p.getErrorStream();
