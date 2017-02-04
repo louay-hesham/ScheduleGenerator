0f1d5a5d666835817b94e4e13a69c8f8a62a42f8
louay-hesham <louay.hesham@gmail.com>
2017-02-04 03:54:59 +0200

    changed file name in file saver

--- src/Core/FileIO/Saver/FileSaver.java
+++ src/Core/FileIO/Saver/FileSaver.java
@@ -13,9 +13,11 @@ import java.util.ArrayList;
  */
 public class FileSaver {
 
-    protected final StringBuilder sb;
+    private final StringBuilder sb;
+    private final String type;
 
     public FileSaver(ArrayList<SubjectPanel> subjects, String type){
+        this.type = type;
         this.sb = new StringBuilder();
         this.sb.append(type + "\r\n");
         this.sb.append(subjects.size() + "\r\n\r\n");
@@ -29,7 +31,7 @@ public class FileSaver {
         if (fileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
             File file = fileChooser.getSelectedFile();
             try {
-                PrintWriter writer = new PrintWriter(file);
+                PrintWriter writer = new PrintWriter(file.getPath() + "_" + this.type + ".SGEN");
                 writer.write(this.sb.toString());
                 writer.close();
             } catch (FileNotFoundException e) {
